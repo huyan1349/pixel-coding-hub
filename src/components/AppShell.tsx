5b288696efa6b803dcd0598d8bde0a4ba488b584
huyan <2971427998@qq.com>
2026-05-31 03:42:56 +0800

    feat: add TopBar, AppShell, MainStage layout and assemble App

---
+++ src/components/AppShell.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from 'react';
+import { TopBar } from './TopBar';
+
+export function AppShell({ sidebar, main, bottom }: { sidebar: ReactNode; main: ReactNode; bottom: ReactNode }) {
+  return (
+    <div className="w-screen h-screen bg-pixel-bg text-pixel-text flex flex-col overflow-hidden select-none">
+      <TopBar />
+      <div className="flex-1 grid grid-cols-12 overflow-hidden">
+        <aside className="col-span-12 md:col-span-3 border-r-0 md:border-r-2 border-pixel-text bg-pixel-dark p-3 overflow-y-auto">
+          {sidebar}
+        </aside>
+        <main className="col-span-12 md:col-span-9 flex flex-col overflow-hidden bg-pixel-bg">
+          <div className="flex-1 overflow-hidden relative">{main}</div>
+          <div className="h-48 border-t-2 border-pixel-text bg-pixel-dark">{bottom}</div>
+        </main>
+      </div>
+    </div>
+  );
+}
